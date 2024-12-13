fd3afd73cdb410ec9bb85e62c5ec13b7f0bb4926
DeepChirp <66902050+DeepChirp@users.noreply.github.com>
2024-12-13 15:20:53 +0800

    style: 根据`.clang-format`统一代码格式

--- ComputationalGeometry/ConvexHull.c
+++ ComputationalGeometry/ConvexHull.c
@@ -3,13 +3,12 @@
  * description: 给定平面上的n个点，求这些点的凸包点集
  */
 
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 // 定义 点 结构体
-typedef struct _point
-{
+typedef struct _point {
     long long x, y;
 } point;
 
@@ -19,20 +18,17 @@ int top = -1;
 point min = (point){0x7fffffffll, 0x7fffffffll};
 
 // 叉积
-long long cross(point s, point a, point b)
-{
+long long cross(point s, point a, point b) {
     return (a.x - s.x) * (b.y - s.y) - (a.y - s.y) * (b.x - s.x);
 }
 
 // 两点间距离的平方
-long long dist(point a, point b)
-{
+long long dist(point a, point b) {
     return (a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y);
 }
 
 // 比较函数 - 按极角排序
-int cmp(const void *a, const void *b)
-{
+int cmp(const void *a, const void *b) {
     const point *p1 = (const point *)a;
     const point *p2 = (const point *)b;
     long long c = cross(min, *p1, *p2);
@@ -44,12 +40,10 @@ int cmp(const void *a, const void *b)
 }
 
 // 找凸包 - stack中剩余元素即为凸包点集
-void convexHull(point *p, int n, point *stack)
-{
+void convexHull(point *p, int n, point *stack) {
     top = -1;
     min = (point){0x7fffffffll, 0x7fffffffll};
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++) {
         scanf("%lld%lld", &p[i].x, &p[i].y);
         // 找到最下最左的点
         if (p[i].y < min.y || (p[i].y == min.y && p[i].x < min.x))
@@ -60,8 +54,7 @@ void convexHull(point *p, int n, point *stack)
     // 栈初始化
     stack[++top] = p[0];
     stack[++top] = p[1];
-    for (int i = 2; i < n; i++)
-    {
+    for (int i = 2; i < n; i++) {
         // 右转则出栈
         while (top > 0 && cross(stack[top - 1], stack[top], p[i]) <= 0)
             top--;
--- MaxFlow/Dinic.cpp
+++ MaxFlow/Dinic.cpp
@@ -2,9 +2,11 @@
  * name: Dinic 算法
  * description:
  *
- * Dinic 算法是求解网络流问题的一种算法，是 Edmonds-Karp 算法的优化版本。Dinic 算法的时间复杂度为 O(V^2 * E)。
+ * Dinic 算法是求解网络流问题的一种算法，是 Edmonds-Karp 算法的优化版本。Dinic
+ * 算法的时间复杂度为 O(V^2 * E)。
  *
- * Dinic 算法的基本思想是通过多次广度优先搜索（BFS）找到层次图，然后通过多次深度优先搜索（DFS）找到增广路径，最终求解出最大流。
+ * Dinic
+ * 算法的基本思想是通过多次广度优先搜索（BFS）找到层次图，然后通过多次深度优先搜索（DFS）找到增广路径，最终求解出最大流。
  */
 
 #include <cstdint>
@@ -18,8 +20,7 @@ using namespace std;
 const int MAX_NUM = 105;
 const int64_t INF = 1e18;
 
-struct Edge
-{
+struct Edge {
     int to, rev;
     int64_t cap;
 };
@@ -28,26 +29,21 @@ vector<Edge> G[MAX_NUM];
 int level[MAX_NUM];
 int iter[MAX_NUM];
 
-void add_edge(int from, int to, int64_t cap)
-{
+void add_edge(int from, int to, int64_t cap) {
     G[from].push_back({to, (int)G[to].size(), cap});
     G[to].push_back({from, (int)G[from].size() - 1, 0});
 }
 
-auto bfs(int s, int t) -> bool
-{
+auto bfs(int s, int t) -> bool {
     memset(level, -1, sizeof(level));
     queue<int> que;
     level[s] = 0;
     que.push(s);
-    while (!que.empty())
-    {
+    while (!que.empty()) {
         int v = que.front();
         que.pop();
-        for (const auto &e : G[v])
-        {
-            if (e.cap > 0 && level[e.to] < 0)
-            {
+        for (const auto &e : G[v]) {
+            if (e.cap > 0 && level[e.to] < 0) {
                 level[e.to] = level[v] + 1;
                 que.push(e.to);
             }
@@ -56,20 +52,15 @@ auto bfs(int s, int t) -> bool
     return level[t] != -1;
 }
 
-auto dfs(int v, int t, int64_t f) -> int64_t
-{
-    if (v == t)
-    {
+auto dfs(int v, int t, int64_t f) -> int64_t {
+    if (v == t) {
         return f;
     }
-    for (int &i = iter[v]; i < G[v].size(); ++i)
-    {
+    for (int &i = iter[v]; i < G[v].size(); ++i) {
         Edge &e = G[v][i];
-        if (e.cap > 0 && level[v] < level[e.to])
-        {
+        if (e.cap > 0 && level[v] < level[e.to]) {
             int64_t d = dfs(e.to, t, min(f, e.cap));
-            if (d > 0)
-            {
+            if (d > 0) {
                 e.cap -= d;
                 G[e.to][e.rev].cap += d;
                 return d;
@@ -79,32 +70,26 @@ auto dfs(int v, int t, int64_t f) -> int64_t
     return 0;
 }
 
-auto max_flow(int s, int t) -> int64_t
-{
+auto max_flow(int s, int t) -> int64_t {
     int64_t flow = 0;
-    while (bfs(s, t))
-    {
+    while (bfs(s, t)) {
         memset(iter, 0, sizeof(iter));
         int64_t f;
-        while ((f = dfs(s, t, INF)) > 0)
-        {
+        while ((f = dfs(s, t, INF)) > 0) {
             flow += f;
         }
     }
     return flow;
 }
 
-auto main() -> int
-{
+auto main() -> int {
     // n: 点数 m: 边数 s: 源点 t: 汇点
     int n, m, s, t;
     cin >> n >> m >> s >> t;
-    for (int i = 1; i <= n; ++i)
-    {
+    for (int i = 1; i <= n; ++i) {
         G[i].clear();
     }
-    for (int i = 0; i < m; ++i)
-    {
+    for (int i = 0; i < m; ++i) {
         int u, v;
         int64_t w;
         cin >> u >> v >> w;
